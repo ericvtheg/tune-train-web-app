795e9090613ac9061c2bd7c42d3bd04b3342a1f8
Eric Ventor <ericventor97@gmail.com>
2022-03-12 15:56:55 -0700

    added request logging middle ware

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,8 +1,13 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { GlobalGuard } from './guards/global.guard';
+import { LoggingMiddleware } from './middleware/logging.middleware';
 
 @Module({
   providers: [{ provide: APP_GUARD, useClass: GlobalGuard }],
 })
-export class CommonModule {}
+export class CommonModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggingMiddleware).forRoutes('*');
+  }
+}
---
+++ src/common/middleware/logging.middleware.ts
@@ -0,0 +1,14 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { nanoid } from 'nanoid';
+
+@Injectable()
+export class LoggingMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: () => void) {
+    const nanoId = nanoid(5);
+    const timeString = `Request-response time ${nanoId} ${req.method} - ${req.originalUrl}`;
+    console.time(timeString);
+    res.on('finish', () => console.timeEnd(timeString));
+    next();
+  }
+}
