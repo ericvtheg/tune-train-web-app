90399ab20648f4fd613d381e71894bc1df5f8038
Eric Ventor <ericventor97@gmail.com>
2022-08-26 17:01:26 -1000

    github actions updates

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Tune Train (WIP)
 
 A webapp built to find and connect with upcoming artists!
+
+<!-- TODO add cloud architecture diagram -->
+<!-- TODO add tech stack -->
--- server/deploy/providers.tf
+++ server/deploy/providers.tf
@@ -23,7 +23,9 @@ provider "aws" {
   region = var.aws_region
 }
 
-module "ecs_vpc" {
+### VPC
+
+module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "${local.prefix}-vpc"
@@ -40,11 +42,54 @@ module "ecs_vpc" {
   tags = local.common_tags
 }
 
+### ECR
+
 resource "aws_ecr_repository" "tune-train" {
-  name                 = "tune-train"
+  name                 = "${local.prefix}-repo-${var.stage}"
   image_tag_mutability = "MUTABLE"
+  tags = local.common_tags
+  force_delete = true
 
   image_scanning_configuration {
     scan_on_push = true
   }
+}
+
+### ECS
+
+### RDS
+
+resource "aws_db_subnet_group" "tune-train-db-subnet-group" {
+  name = "${local.prefix}-db-subnet-group"
+
+  subnet_ids = module.vpc.private_subnets
+
+  tags = local.common_tags
+}
+
+resource "aws_db_instance" "tune-train" {
+  depends_on = [module.vpc, resource.aws_db_subnet_group.tune-train-db-subnet-group]
+
+  allocated_storage           = 5
+  max_allocated_storage       = 20
+  allow_major_version_upgrade = false
+  identifier = "${local.prefix}-${var.stage}"
+  availability_zone           = var.azs[0]
+  engine                      = "postgres"
+  engine_version              = "14.4"
+  instance_class              = "db.t4g.micro"
+  skip_final_snapshot         = true
+  tags                        = local.common_tags
+  performance_insights_enabled = true
+  performance_insights_retention_period = 7
+
+  db_name  = var.db_name
+  username = var.db_username
+  password = var.db_password
+  port     = var.db_port
+
+
+  db_subnet_group_name = aws_db_subnet_group.tune-train-db-subnet-group.name
+  # vpc_security_group_ids = [module.vpc.vpc_id] TODO add security groups
+  # security_group_names =
 }
