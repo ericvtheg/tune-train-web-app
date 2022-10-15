7faec07cbea14fd134b8b92ccfec0cbb47583336
Eric Ventor <ericventor97@gmail.com>
2022-10-14 22:29:55 -0700

    added seeding

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -6,6 +6,23 @@ async function main() {
   // await prisma.user.deleteMany();
   // await prisma.post.deleteMany();
 
+  await prisma.user.create({
+    data: {
+      email: "testemail@gmail.com",
+      username: "testusername",
+      password: "somePassword",
+      first_name: "Firstname",
+      last_name: "Lastname",
+      artist: {
+        create: {
+          stage_name: "Some Stage Name",
+          bio: "some bio",
+          image: "someimageurl.com/image"
+        }
+      }
+    }
+  })
+
   console.log('Seeding...');
 
   // create data here
