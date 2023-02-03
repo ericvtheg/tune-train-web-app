43758802ad9306c7450d02c040d456209aaa5f37
Eric Ventor <ericventor97@gmail.com>
2023-02-02 21:30:26 -0800

    fixed local auth guard bug

--- server/src/common/guards/local-auth.guard.ts
+++ server/src/common/guards/local-auth.guard.ts
@@ -5,10 +5,12 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 @Injectable()
 export class LocalAuthGuard extends AuthGuard('local') {
   getRequest(context: ExecutionContext) {
+    console.log('hit in local auth guard');
     const ctx = GqlExecutionContext.create(context);
     const req = ctx.getContext().req;
     const args = ctx.getArgs();
-    req.body = args.userLoginData;
+    // puts graphql input arg into req.body
+    req.body = args.input;
     return req;
   }
 }
